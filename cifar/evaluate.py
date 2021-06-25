a056636986c1f4972271aa890e3d570357dbb7f7
HAXRD@lyus <haxrd@stu.xmu.edu.cn>
2021-06-25 10:06:03 +0800

    Fix a bug

--- cifar/evaluate.py
+++ cifar/evaluate.py
@@ -81,7 +81,7 @@ if args.cuda:
 kwargs = {'num_workers': 1, 'pin_memory': True} if args.cuda else {}
 if args.dataset == 'cifar10':
     test_loader = torch.utils.data.DataLoader(
-        datasets.CIFAR10('./data.cifar10', train=False, transform=transforms.Compose([
+        datasets.CIFAR10('./data.cifar10', download=True, train=False, transform=transforms.Compose([
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
         ])),
